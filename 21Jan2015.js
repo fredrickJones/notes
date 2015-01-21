8ce2abe65d2fdbc858728e1793ae1aa050ced0ac
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-21 12:03:59 -0700

    update

---
+++ 21Jan2015.js
@@ -0,0 +1,35 @@
+//HTTP: Hypertext Transfer Protocol
+  //request made to
+    //URL
+  //requests and responses both have
+    //headers
+    //body
+    //cookies
+
+//JSON
+  //JavaScript Object Notation
+  //JSON can write to JS but Js cann't write to JSON
+  //things listed in JSON
+    //string
+    //boolean
+    //object
+    //
+
+//AJAX
+  //Asynchronous Javascript and XML
+
+//ReST (or REST)
+  //Representation State Transfer
+  //API aplication programming interface
+  //
+  //Most used Methods
+    //Create - post
+    //Read - get
+    //Update - put
+    //Destroy - delete
+
+
+
+
+
+
