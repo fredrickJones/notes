27aaee92f874d13afe9d5b4def82105dde0e8b4e
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-22 12:17:12 -0700

    updated

---
+++ 22Jan2015.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// angularJS native service libraries
+  //$http
+  //$q
+  //$timeout
+  //$interval
+  //
+
+//CORS - Cross Origin Resource(Request) Sharing
+  //backed into all browsers
+  
