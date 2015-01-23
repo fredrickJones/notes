a575b3a050c71c171bceb3ce8401082b4de961f5
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-23 13:54:46 -0700

    updated

---
+++ 23Jan2015.js
@@ -0,0 +1,76 @@
+//AngularJS Review
+
+
+//module.controller
+//module.directive
+//module.service
+//module.filter
+
+
+//CONTROLLERS
+  //the controller mediates between the data(services) & the view(html)
+  //the two most important things within coding
+    //MODULARITY
+  	//DONT DUPLICATE
+ //populates the scope
+
+//SERVICES
+  //the controller brings services into play
+  //inject services into the controller function
+  //anything that starts with the '$' is a built in Angular services
+  //custom services are just injected into the controller by name of service
+  //their job is to be your 'business logic'
+    //doing something to data, with data 
+    //complex coding should not be done in the controller but instead in the service
+  //put the logic in the right place and save yourself hours and hours of change.
+
+//DIRECTIVES
+  //modularize view functionality
+  //more on Tuesday(27 Jan)
+
+//FILTERS
+  //
+
+
+//CODE SMELL
+  //when something seems a bit off and you need to rethink how you are doing it
+  //i.e. number of parameters reaching 6-7;
+
+
+//COMMENTS ARE BAD
+  //a CODE SMELL or failures to craft your code correctly
+  //its okay now but stop using them soon
+  //
+
+//your career will be 30% education
+
+
+//for 99.9% of projects a single module will work
+
+
+//ways to figure out where your code broke
+  //hard code
+    //this isolates the input of the different moving parts
+
+  //console.log
+    //this isolates the output of the different moving parts
+
+
+//JavaScript is actually called ECMAScript
+  //the next release is now ECMAScript2015 not ES6.
+
+
+//for promises
+  //invoke the function
+    //itunes.getData().then(success, failure)
+
+
+//premature optimization is the route to bad coding
+
+
+
+
+
+
+
+
