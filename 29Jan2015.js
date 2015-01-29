3b0a39c769ac241f57b1571da94f444805fba056
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-29 16:42:46 -0700

    updated

---
+++ 29Jan2015.js
@@ -0,0 +1,25 @@
+// parse and firebase are backend for a service
+
+// parse.com/docs/rest
+  // make an account
+  // got to dashboard
+  // create a new app
+  // coge and keys
+
+  // you need app id
+  // API key
+  // put 
+
+// FIREBASE
+  // this is a no seql data store
+  // real time 
+  // uses web sockets (a ping)
+    // live constent updated
+  // @ https://www.firebase.com/docs/web/guide/
+    // read 1-7
+
+// you always want a server to handle the secure portions of your app
+  // you only need a little portion for your
+// nothing in the browser is secret/secure
+  // NEVER PUT ANYTHING SECRET ON THE BROWSER
+// 
