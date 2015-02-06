b48db45320188fad0a2f948cb6826bba088f6df5
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-02-06 15:18:55 -0700

    update

---
+++ 6Feb2015.js
@@ -0,0 +1,9 @@
+// LEARNING EXPRESS
+
+// npm - NODE PACKAGE MANAGER
+
+// create your directory
+// create your .json file
+// add {}
+// type npm init
+// npm install -- save (adds dependencies to the directory)
